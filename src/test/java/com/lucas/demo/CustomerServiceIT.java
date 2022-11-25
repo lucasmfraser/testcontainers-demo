75252702a24525fef66aa9ed8e4e5350df4966be
Lucas Fraser <lucasfraser@hotmail.co.uk>
2022-11-25 14:41:34 +0000

    integrate with testcontainers

--- src/test/java/com/lucas/demo/CustomerServiceIT.java
+++ src/test/java/com/lucas/demo/CustomerServiceIT.java
@@ -7,17 +7,34 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class CustomerServiceIT {
 
+    @Container
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+
+    @DynamicPropertySource
+    private static void overrideProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", mySQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
     @Autowired
-    private CustomerRepository customerRepository;
+    private CustomerService customerService;
 
     @Test
     void shouldReturnAllCustomersWhenCallingFindAll() {
-        List<Customer> customers = ImmutableList.copyOf(customerRepository.findAll());
-        assertThat(customers).isEmpty();
+        List<Customer> customers = ImmutableList.copyOf(customerService.findAllOrders());
+        assertThat(customers).isNotEmpty().hasSize(2);
     }
 
 }
